8fbb50e431f8976c6e0a3155a25a8e2442d6b044
André Fretta <andrefretta@gmail.com>
2023-02-03 16:41:32 -0300

    feat: cria rotas GET /people e GET /people/:id .

--- src/routes/peopleRoutes.js
+++ src/routes/peopleRoutes.js
@@ -3,6 +3,31 @@ const peopleDB = require('../db/peopleDB');
 
 const router = express.Router();
 
+router.get('/', async (_req, res) => {
+  try {
+    const [result] = await peopleDB.findAll();
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.sqlMessage });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [[result]] = await peopleDB.findById(id);
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json({ message: 'Pessoa não encontrada' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.sqlMessage });
+  }
+});
+
 router.post('/', async (req, res) => {
   const person = req.body;
   try {
