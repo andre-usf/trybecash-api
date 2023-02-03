63c0df923d3bd495a05afba647f0f1170027bcec
André Fretta <andrefretta@gmail.com>
2023-02-03 09:52:22 -0300

    refactor: retira teste de conexão com o sql no server.

--- src/server.js
+++ src/server.js
@@ -5,9 +5,4 @@ const PORT = 3001;
 
 app.listen(PORT, async () => {
   console.log(`API TrybeCash está sendo executada na porta ${PORT}`)
-
-  const [result] = await connection.execute('SELECT 1');
-  if (result) {
-    console.log('MySQL connection OK');
-  }
 });
