131e9ab3ab16dcd8d6fab9ac0a5cae9e4366398b
André Fretta <andrefretta@gmail.com>
2023-02-04 10:58:59 -0300

    test: cria testes para os endpoints  PUT /people/:id e DELETE /people/:id .

--- tests/integration/people.test.js
+++ tests/integration/people.test.js
@@ -5,9 +5,9 @@ const sinon = require('sinon');
 const app = require('../../src/app');
 const connection = require('../../src/db/connection');
 
-const { expect, use } = chai;
+const { expect } = chai;
 
-use(chaiHttp);
+chai.use(chaiHttp);
 
 const peopleList = [
   {
@@ -30,31 +30,24 @@ describe('Testando os endpoins de people', function () {
   it('Testando o cadastro de uma pessoa', async function () {
     sinon.stub(connection, 'execute').resolves([{ insertId: 42 }])
 
-    const response = await chai
-      .request(app)
-      .post('/people')
-      .send({
+    const response = await chai.request(app).post('/people').send({
         firstName: 'Luke',
         lastName: 'Skywalker',
         email: 'luke.skywalker@trybe.com',
         phone: '851 678 4453',
-      },
-      );
+      },);
 
       expect(response.status).to.equal(201);
       expect(response.body).to.deep.equal({ 
         message: 'Pessoa cadastrada com sucesso com o id 42'
       });
     
-    afterEach(sinon.restore);
   });
 
   it('Testando a listagem de todas as pessoas', async function () {
     sinon.stub(connection, 'execute').resolves([peopleList]);
 
-    const response = await chai
-      .request(app)
-      .get('/people');
+    const response = await chai.request(app).get('/people');
 
     expect(response.status).to.equal(200);
     expect(response.body).to.deep.equal(peopleList);
@@ -63,11 +56,36 @@ describe('Testando os endpoins de people', function () {
   it('Testando a listagem da pessoa com o id 1', async function () {
     sinon.stub(connection, 'execute').resolves([[peopleList[0]]]);
 
-    const response = await chai
-      .request(app)
-      .get('/people/1');
+    const response = await chai.request(app).get('/people/1');
 
     expect(response.status).to.equal(200);
     expect(response.body).to.deep.equal(peopleList[0]);
   });
+
+  it('Testando a alteração de uma pessoa com o id 1', async function () {
+    sinon.stub(connection, 'execute').resolves([{ affectedRows: 1 }]);
+    
+    const response = await chai.request(app).put('/people/1').send({
+      firstName: 'Lucão',
+      lastName: 'Andarilho dos céus',
+      email: 'lucao.andarilho@trybe.com',
+      phone: '851 678 4453',
+    },);
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal({ message: 'Pessoa de id 1 atualizada com sucesso' })
+  });
+
+  it('Testando a exclusão da pessoa com id 1', async function () {
+    sinon.stub(connection, 'execute').resolves([{ affectedRows: 1 }]);
+    const response = await chai
+      .request(app)
+      .delete('/people/1');
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to
+      .deep.equal({ message: 'Pessoa de id 1 excluída com sucesso' });
+  });
+
+  afterEach(sinon.restore);
 });
