d13eeb719540869074b583357917d9b87e6f52d6
André Fretta <andrefretta@gmail.com>
2023-02-04 11:37:21 -0300

    feat: cria rotas PUT /people/:id e DELETE /people/:id // fix: adiciona retornos nas funções.

--- src/routes/peopleRoutes.js
+++ src/routes/peopleRoutes.js
@@ -6,10 +6,10 @@ const router = express.Router();
 router.get('/', async (_req, res) => {
   try {
     const [result] = await peopleDB.findAll();
-    res.status(200).json(result);
+    return res.status(200).json(result);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: error.sqlMessage });
+    return res.status(500).json({ message: error.sqlMessage });
   }
 });
 
@@ -18,13 +18,13 @@ router.get('/:id', async (req, res) => {
     const { id } = req.params;
     const [[result]] = await peopleDB.findById(id);
     if (result) {
-      res.status(200).json(result);
+      return res.status(200).json(result);
     } else {
-      res.status(404).json({ message: 'Pessoa não encontrada' });
+      return res.status(404).json({ message: 'Pessoa não encontrada' });
     }
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: error.sqlMessage });
+    return res.status(500).json({ message: error.sqlMessage });
   }
 });
 
@@ -32,12 +32,40 @@ router.post('/', async (req, res) => {
   const person = req.body;
   try {
     const [result] = await peopleDB.insert(person);
-    res.status(201).json({ message: `Pessoa cadastrada com sucesso com o id ${result.insertId}` });
+    return res.status(201).json({ message: `Pessoa cadastrada com sucesso com o id ${result.insertId}` });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: 'Ocorreu um erro ao cadastrar uma pessoa' })
+    return res.status(500).json({ message: 'Ocorreu um erro ao cadastrar uma pessoa' })
   }
-  
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const person = req.body;
+    const [result] = await peopleDB.update(person, id);
+    if (result.affectedRows > 0) {
+      return res.status(200).json({ message: `Pessoa de id ${id} atualizada com sucesso` });
+    } else {
+      return res.status(404).json({ message: 'Pessoa não encontrada' });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.sqlMessage });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [result] = await peopleDB.remove(id);
+    if (result.affectedRows > 0) {
+      return res.status(200).json({ message: `Pessoa de id ${id} excluída com sucesso` });
+    } else {
+      return res.status(404).json({ message: 'Pessoa não encontrada' });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.sqlMessage });
+  }
 });
 
 module.exports = router;
